a370627556ff4260a21d7e1c0e65695dbdd98105
Maciej Królik <maciejkrolik@outlook.com>
2018-12-01 22:21:39 +0100

    Add Day 1 Part 2 solution

---
+++ src/day1/Part2.kt
@@ -0,0 +1,31 @@
+import java.io.File
+
+fun main() {
+    val file = File("src/day1/part1_input.txt")
+
+    var currentFrequency = 0
+    var found = false
+    val listOfFrequencies: MutableList<Int> = mutableListOf(0)
+
+    while (true) {
+
+        file.useLines { lines ->
+            for (it in lines) {
+                currentFrequency += it.toInt()
+                if (listOfFrequencies.contains(currentFrequency)) {
+                    println(currentFrequency)
+                    found = true
+                    break
+
+                } else {
+                    listOfFrequencies.add(currentFrequency)
+                }
+            }
+        }
+
+        if (found) {
+            return
+        }
+
+    }
+}
